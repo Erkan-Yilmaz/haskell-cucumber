9172e5dc44f9f36419f1504b5ca3919ce199b95f
Sakari Jokinen <sakariij@gmail.com>
2011-09-10 22:59:49 +0300

    Pass step definitions to scenario runner

--- Test/Cucumber.hs
+++ Test/Cucumber.hs
@@ -6,18 +6,32 @@ import Language.Gherkin
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Text.Parsec.String
+import Text.Regex
+import Data.Maybe
+import Data.List
 
-cucumber :: FilePath -> IO [Test]
-cucumber path = either (error . show) cukeFeature `fmap` 
+type StepDefinitions = [StepDefinition]
+data StepDefinition = StepDefinition { step_pattern :: String
+                                     , step_action :: IO ()
+                                     }
+
+cucumber :: StepDefinitions -> FilePath -> IO [Test]
+cucumber steps path = either (error . show) (cukeFeature steps) `fmap` 
                 parseFromFile parseFeature path
     
-cukeFeature :: Feature -> [Test]
-cukeFeature Feature { feature_scenarios }  = concat $ 
-                      cukeScenario `fmap` feature_scenarios
+cukeFeature :: StepDefinitions -> Feature -> [Test]
+cukeFeature steps Feature { feature_scenarios }  = concat $ 
+                      cukeScenario steps `fmap` feature_scenarios
     
-cukeScenario :: Scenario -> [Test]    
-cukeScenario Scenario { scenario_name
-                      , scenario_steps } = [testCase scenario_name $ mapM_ executeStep scenario_steps]  
+cukeScenario :: StepDefinitions -> Scenario -> [Test]    
+cukeScenario steps Scenario { scenario_name
+                      , scenario_steps } = [testCase scenario_name $ mapM_ (executeStep steps) scenario_steps]  
 
-executeStep :: Step -> IO ()
-executeStep step = step_text step 
+executeStep :: StepDefinitions -> Step -> IO ()
+executeStep steps step = pickStep steps step 
+
+pickStep :: StepDefinitions -> Step -> IO ()
+pickStep steps step = fromMaybe notFound $ step_action `fmap` find go steps
+  where
+    notFound = error $ "Could not find a step definition.\nYou need to implement it before proceeding"
+    go StepDefinition { step_pattern } = isJust $ mkRegex step_pattern `matchRegex` step_body (step_text step)
