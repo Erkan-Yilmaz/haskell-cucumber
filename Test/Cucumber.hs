921f60cb5e3e912f0927097ae45325411f944516
Sakari Jokinen <sakariij@gmail.com>
2011-09-10 10:34:29 +0300

    Types seem ok

--- Test/Cucumber.hs
+++ Test/Cucumber.hs
@@ -1,9 +1,30 @@
-module Test.Cucumber (cuke) where
+module Test.Cucumber where
 
-import Language.Gherkin as Gherkin
+import qualified Language.Gherkin as Gherkin
 import Test.Framework
 
-data Configuration = Configuration
+data Arg = Pystring String
+         | Table [[String]]
 
-cuke :: Configuration -> Gherkin.AST -> [Test]
-cuke = error "tbd"
+data CukeM = CukeM
+
+data Step m = Step m
+
+data Runner s m = Runner {
+  run_before :: CukeM s
+  run_step :: s -> m () -> Cuke s
+  run_after :: s -> CukeM ()
+  }
+
+scenario :: CukeM String
+tags :: CukeM [String]
+failScenario :: String -> CukeM a
+isFailing :: CukeM Bool
+liftIO :: IO a -> CukeM a
+
+cuke :: Runner s -> Gherkin.Feature -> [Test]
+ 
+cukeScenario :: Runner s -> Gherkin.Scenario -> [Test]
+
+type StepPattern = String
+step :: StepPattern -> [String] -> Maybe Arg -> m () -> Step m
