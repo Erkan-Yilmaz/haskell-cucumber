c0abef988c9fcff3e0da55d0e5d811b3b3fef5cc
Sakari Jokinen <sakariij@gmail.com>
2011-09-10 22:43:17 +0300

    saved state

--- Test/Cucumber.hs
+++ Test/Cucumber.hs
@@ -1,30 +1,23 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Test.Cucumber where
 
-import qualified Language.Gherkin as Gherkin
+import Language.Gherkin
 import Test.Framework
+import Test.Framework.Providers.HUnit
+import Text.Parsec.String
 
-data Arg = Pystring String
-         | Table [[String]]
+cucumber :: FilePath -> IO [Test]
+cucumber path = either (error . show) cukeFeature `fmap` 
+                parseFromFile parseFeature path
+    
+cukeFeature :: Feature -> [Test]
+cukeFeature Feature { feature_scenarios }  = concat $ 
+                      cukeScenario `fmap` feature_scenarios
+    
+cukeScenario :: Scenario -> [Test]    
+cukeScenario Scenario { scenario_name
+                      , scenario_steps } = [testCase scenario_name $ mapM_ executeStep scenario_steps]  
 
-data CukeM = CukeM
-
-data Step m = Step m
-
-data Runner s m = Runner {
-  run_before :: CukeM s
-  run_step :: s -> m () -> Cuke s
-  run_after :: s -> CukeM ()
-  }
-
-scenario :: CukeM String
-tags :: CukeM [String]
-failScenario :: String -> CukeM a
-isFailing :: CukeM Bool
-liftIO :: IO a -> CukeM a
-
-cuke :: Runner s -> Gherkin.Feature -> [Test]
- 
-cukeScenario :: Runner s -> Gherkin.Scenario -> [Test]
-
-type StepPattern = String
-step :: StepPattern -> [String] -> Maybe Arg -> m () -> Step m
+executeStep :: Step -> IO ()
+executeStep step = step_text step 
